1d59d352bbdf3aaab19c9a3559ed8579e749b91f
prakharVats <vatsprakhar91@gmail.com>
2024-09-21 15:16:43 +0530

    initial  commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const Car = function(car , speed){
+    this.car = car;
+    this.speed = speed;
+}
+
+
+Car.prototype.chargeBattery = function(changeTo){
+    this.charge = changeTo;
+    console.log(`charged to : ${this.charge}%`);
+}
+Car.prototype.accelerate = function(){
+this.speed += 20;
+this.charge -= 1;
+console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
+}
+Car.prototype.break = function(){
+    this.speed -= 10;
+    this.charge -=2;
+    console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
+
+}
+
+
+//  EV CAR
+const EV = function(car , speed , charge){
+    Car.call(this , car , speed , charge);
+    this.charge = charge;
+}
+// linking EV with Car
+EV.prototype = Object.create(Car.prototype);
+
+
+
+const BMW = new EV("BMW" , 150 , "24");
+BMW.accelerate();
+BMW.accelerate();
+BMW.break();
+BMW.chargeBattery(50);
+BMW.accelerate();
+BMW.break();
+
