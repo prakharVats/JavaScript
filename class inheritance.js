c59b4e5393454198cecafd8e50bf49b7a3da0ffa
prakharVats <vatsprakhar91@gmail.com>
2024-09-21 15:29:18 +0530

    update

--- class inheritance.js
+++ class inheritance.js
@@ -7,15 +7,13 @@ const Car = function(car , speed){
 
 
 Car.prototype.accelerate = function(){
-this.speed += 20;
-this.charge -= 1;
-console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
+this.speed += 10;
+
+console.log(`Your ${this.car} is running at ${this.speed}`)
 }
 Car.prototype.break = function(){
-    this.speed -= 10;
-    this.charge -=2;
-    console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
-
+    this.speed -= 5;
+    console.log(`Your ${this.car} is running at ${this.speed}`)
 }
 
 
@@ -32,7 +30,19 @@ EV.prototype.chargeBattery = function(changeTo){
     this.charge = changeTo;
     console.log(`charged to : ${this.charge}%`);
 }
+//  this accelerate method will overwrite the accelerate method of the Car class -- same goes for the break method
+EV.prototype.accelerate = function(){
+    this.speed += 20;
+    this.charge -= 1;
+    console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
+    }
 
+EV.prototype.break = function(){
+        this.speed -= 10;
+        console.log(`Your ${this.car} is running at ${this.speed}, with a charge of ${this.charge}%`)
+    }
+
+    
 const BMW = new EV("BMW" , 150 , "24");
 BMW.accelerate();
 BMW.accelerate();
