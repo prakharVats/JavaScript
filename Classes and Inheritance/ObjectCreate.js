b7f1fc3c91f241269f782dcd98a14287b4cfbafa
prakharVats <vatsprakhar91@gmail.com>
2024-09-21 16:12:27 +0530

    object create

--- Classes and Inheritance/ObjectCreate.js
+++ Classes and Inheritance/ObjectCreate.js
@@ -1,1 +1,27 @@
 console.log("create object");
+
+const PersonProto = {
+    calcAge(){
+        console.log(2040 - this.birthYear);
+    },
+    init(firstName , birthYear){
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+}
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName , birthYear , course){
+    PersonProto.init.call(this , firstName , birthYear);
+    this.course = course;
+
+}
+
+StudentProto.introduce = function(){
+console.log("hello this is : " , this.firstName);
+}
+
+const nick = Object.create(StudentProto);
+nick.init("Nick Jr" , 2002 , "CSE")
+nick.introduce();
+nick.calcAge();
